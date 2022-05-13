6cd05d20ae5c04ffa928e5ca5e529377f071c817
jonathanmperes <emailmarceloperes@gmail.com>
2022-05-13 13:19:01 -0300

    Buscando o menor valor

--- src/main/kotlin/Produto.kt
+++ src/main/kotlin/Produto.kt
@@ -1,2 +1,6 @@
-class Produto {
+data class Produto(
+    val nome: String = "",
+    val preco: Double = 0.0
+) {
+
 }
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,23 @@
+fun main() {
+    val produtos = arrayOf<Produto>(
+        Produto(nome = "Lamborguini", preco = 1000000.0),
+        Produto(nome = "Jipe", preco = 46000.0),
+        Produto(nome = "Brasília", preco = 16000.0),
+        Produto(nome = "Smart", preco = 46000.0),
+        Produto(nome = "Fusca", preco = 17000.0)
+    )
+    var maisBarato = Double.MAX_VALUE
+    for (produto in produtos) {
+        if (produto.preco < maisBarato) {
+            maisBarato = produto.preco
+        }
+    }
+    println(maisBarato)
+
+    produtos.forEach { produto ->
+        if (produto.preco < maisBarato) {
+            produto.preco.also { maisBarato = it }
+        }
+    }
+    println(maisBarato)
+}
